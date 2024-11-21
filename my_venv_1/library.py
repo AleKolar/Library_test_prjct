6c420dcd211755d0f4483646a5c4a9ba662f2d83
AleKolar <gefest-173@rambler.ru>
2024-11-21 19:34:28 +0300

    beggining

---
+++ my_venv_1/library.py
@@ -0,0 +1,46 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.id = id(self)
+        self.title = title
+        self.author = author
+        self.year = year
+        self.status = "в наличии"
+
+
+class Library:
+    def __init__(self):
+        self.books = {}
+
+    def add_book(self, title, author, year):
+        book = Book(title, author, year)
+        self.books[book.id] = book
+
+    def remove_book(self, book_id):
+        if book_id in self.books:
+            del self.books[book_id]
+        else:
+            print("Book not found")
+
+    def search_book(self, search_term):
+        found_books = []
+        for book_id, book in self.books.items():
+            if search_term in [book.title, book.author, str(book.year)]:
+                found_books.append(book)
+        return found_books
+
+    def display_books(self):
+        for book_id, book in self.books.items():
+            print(
+                f"ID: {book.id}, Title: {book.title}, Author: {book.author}, Year: {book.year}, Status: {book.status}")
+
+    def change_status(self, book_id, new_status):
+        if book_id in self.books:
+            current_status = self.books[book_id].status
+            if current_status == "в наличии" and new_status == "выдана":
+                self.books[book_id].status = new_status
+            elif current_status == "выдана" and new_status == "в наличии":
+                self.books[book_id].status = new_status
+            else:
+                print("Invalid status change. The book is currently " + current_status)
+        else:
+            print("Book not found")
